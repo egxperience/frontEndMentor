ef2a2d43bd2f8c825b77038ed74d310cc45fca57
egxperience <egxperience@gmail.com>
2021-01-28 18:08:15 +0100

    Initial project uploads
    FAQ-Accordion and Landing page example

---
+++ faq-accordion-card/script.js
@@ -0,0 +1,17 @@
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+    const title = question.querySelector(".question-title");
+    title.addEventListener("click", function () {
+    questions.forEach(function (item) {
+        if (item !== question) {
+        item.classList.remove("show-text");
+        }
+    });
+    question.classList.toggle("show-text");
+    });
+});
+
+
+
